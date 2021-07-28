5c208d0c0b2a1d114ecac94d80483ed36aa36016
zhangjunjun <zhangjunjun@weibopay.com>
2021-07-28 11:47:25 +0800

    优化整数反转算法

--- LearnSwift/SwiftBooks/Controller/LeetCode/SWSWReverseIntegerVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWSWReverseIntegerVC.swift
@@ -21,9 +21,9 @@ class SWSWReverseIntegerVC: SWBaseViewController {
      题目描述:整数反转
      
      给你一个 32 位的有符号整数 x ，返回将 x 中的数字部分反转后的结果。
-
+     
      如果反转后整数超过 32 位的有符号整数的范围 [−231,  231 − 1] ，就返回 0。
-
+     
      来源：力扣（LeetCode）
      链接：https://leetcode-cn.com/problems/reverse-integer
      著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -83,6 +83,26 @@ class SWSWReverseIntegerVC: SWBaseViewController {
             return 0
         }
         let result = isPositive ?  numberResult : -numberResult
+        return result
+    }
+    
+    
+    /// 整数反转优化
+    /// - Parameter x: 输入参数
+    /// - Returns: 输出参数
+    func reverIntegerImprove(_ x:Int) -> Int {
+        var input = x;
+        var result = 0
+        while input != 0 {
+            let tempResult = input % 10
+            input = input / 10
+            result = result * 10 + tempResult
+        }
+        if (result > INT32_MAX) {
+            return 0
+        } else if (result < -INT32_MAX - 1) {
+            return 0
+        }
         return result
     }
     
