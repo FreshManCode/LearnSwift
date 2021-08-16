d91f40910338873d22f5ffe83447df364aba3ec2
zhangjunjun <zhangjunjun@weibopay.com>
2021-08-16 11:18:16 +0800

    Leetcode_自底向上的递归

--- LearnSwift/SwiftBooks/Controller/LeetCode/SWBalancedTreeVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWBalancedTreeVC.swift
@@ -10,10 +10,16 @@ import UIKit
 
 class SWBalancedTreeVC: SWBaseViewController {
     
+    var nodeOne:TreeNode?
+    var nodeTwo:TreeNode?
+    var nodeThree:TreeNode?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         p_initDefautData()
+        p_initDefaultNode()
     }
     
     
@@ -23,14 +29,24 @@ class SWBalancedTreeVC: SWBaseViewController {
         listItemArray.append(SWBookListItem(title: "1.平衡二叉树 ",
                                             subTitle: "",
                                             funName: "myIsBalancedTree"))
+        listItemArray.append(SWBookListItem(title: "2.平衡二叉树(自底向上的递归) ",
+                                            subTitle: "",
+                                            funName: "myIsBalancedTreeFromBottomToTop"))
         
         
         
         tableView.reloadData()
     }
     
+    func p_initDefaultNode() {
+//        root = [3,9,20,null,null,15,7]
+        nodeOne = TreeNode(3, TreeNode(9), TreeNode(20, TreeNode(15), TreeNode(7)))
+//        root = [1,2,2,3,3,null,null,4,4]
+        nodeTwo = TreeNode(1, TreeNode(2, TreeNode(3, TreeNode(4), TreeNode(4)), TreeNode(3)), TreeNode(2))
+    }
     
-    func myIsBalancedTree(_ root: TreeNode?) -> Bool {
+    
+    @objc func myIsBalancedTree() {
         /**
          给定一个二叉树，判断它是否是高度平衡的二叉树。
 
@@ -47,7 +63,10 @@ class SWBalancedTreeVC: SWBaseViewController {
          输入：root = []
          输出：true
          */
-        return myCaculateIsBalancedTree(root)
+        let result = myCaculateIsBalancedTree(nodeOne)
+        let result2 = myCaculateIsBalancedTree(nodeTwo)
+        
+        print("result:\(result) result2:\(result2)")
     }
     
     func myCaculateIsBalancedTree(_ root: TreeNode?) -> Bool {
@@ -85,7 +104,36 @@ class SWBalancedTreeVC: SWBaseViewController {
     }
     
     
+    // MARK: - 平衡二叉树(自底向上的递归)
+    @objc func myIsBalancedTreeFromBottomToTop()  {
+        /*
+         方法一由于是自顶向下递归，因此对于同一个节点，函数 height 会被重复调用，导致时间复杂度较高。如果使用自底向上的做法，则对于每个节点，函数 height 只会被调用一次。
+
+         自底向上递归的做法类似于后序遍历，对于当前遍历到的节点，先递归地判断其左右子树是否平衡，再判断以当前节点为根的子树是否平衡。如果一棵子树是平衡的，则返回其高度（高度一定是非负整数），否则返回 -1。如果存在一棵子树不平衡，则整个二叉树一定不平衡。
+         链接：https://leetcode-cn.com/problems/balanced-binary-tree/solution/ping-heng-er-cha-shu-by-leetcode-solution/
+         */
+        let result = myCaculateIsBalancedTree(nodeOne)
+        let result2 = myCaculateIsBalancedTree(nodeTwo)
+        
+        print("result:\(result) result2:\(result2)")
+        
+    }
     
+    func isBalancedTree(_ tree:TreeNode?) -> Bool {
+        return height(tree) >= 0
+    }
     
+    func height(_ tree:TreeNode?) -> Int {
+        if tree == nil {
+            return 0
+        }
+        let leftHeight  = height(tree?.left)
+        let rightHeight = height(tree?.right)
+        if leftHeight == -1 || rightHeight == -1 || abs(leftHeight - rightHeight) > 1 {
+            return -1
+        } else {
+            return max(leftHeight, rightHeight) + 1
+        }
+    }
     
 }
