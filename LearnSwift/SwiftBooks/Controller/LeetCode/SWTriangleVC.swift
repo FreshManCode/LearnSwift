446c231b870227757afc5eaebe939431775d8ec3
zhangjunjun <zhangjunjun@weibopay.com>
2021-08-18 18:08:13 +0800

    LeetCode 买卖股票的最佳时机

--- LearnSwift/SwiftBooks/Controller/LeetCode/SWTriangleVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWTriangleVC.swift
@@ -26,10 +26,13 @@ class SWTriangleVC: SWBaseViewController {
         listItemArray.append(SWBookListItem(title: "2.杨辉三角 II ",
                                             subTitle: "",
                                             funName: "myGetRowTriangle"))
+        listItemArray.append(SWBookListItem(title: "3.买卖股票的最佳时机 ",
+                                            subTitle: "",
+                                            funName: "myMaxProfit"))
         
         
         
-    
+        
         
         tableView.reloadData()
     }
@@ -145,20 +148,89 @@ class SWTriangleVC: SWBaseViewController {
     /// - Parameter rowIndex: 指定索引
     /// - Returns: 返回结果
     func highGetRow(_ rowIndex: Int) -> [Int] {
-            var res = [[Int]]()
-            for i in 0 ... rowIndex {
-                var temp = [Int](repeating: 0, count: i + 1)
-                temp[0] = 1
-                temp[i] = 1
-                if i > 1 {
-                    for j in 1 ..< i {
-                        temp[j] = res[i - 1][j - 1] + res[i - 1][j]
-                    }
+        var res = [[Int]]()
+        for i in 0 ... rowIndex {
+            var temp = [Int](repeating: 0, count: i + 1)
+            temp[0] = 1
+            temp[i] = 1
+            if i > 1 {
+                for j in 1 ..< i {
+                    temp[j] = res[i - 1][j - 1] + res[i - 1][j]
                 }
-                res.append(temp)
             }
-            return res.last!
+            res.append(temp)
         }
+        return res.last!
+    }
+    
+    // MARK: - 买卖股票的最佳时机
+    @objc func myMaxProfit()  {
+        /**
+         定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+         
+         你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+         
+         返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+         
+         链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock
+         
+         
+         示例 1：
+
+         输入：[7,1,5,3,6,4]
+         输出：5
+         解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+              注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+         示例 2：
+
+         输入：prices = [7,6,4,3,1]
+         输出：0
+         解释：在这种情况下, 没有交易完成, 所以最大利润为 0。
+
+         */
+        let inputs1 = [7,1,5,3,6,4]
+        let inputs2 = [3,2,6,5,0,3]
+        
+        print("1:\(myMaxProfit(inputs1))")
+        print("2:\(myMaxProfit(inputs2))")
+        
+    }
+    
+    func myMaxProfit(_ prices: [Int]) -> Int {
+        /**
+         暴力计算法,性能低.可能提交通不过
+         */
+        var maxProfix = 0
+        let maxLength = prices.count
+        for i in 0 ..< maxLength {
+            for j  in i + 1 ..< maxLength {
+                maxProfix = max(maxProfix, prices[j] - prices[i])
+            }
+        }
+        return maxProfix
+    }
+    
+    func modifyMaxProfit(_ prices: [Int]) -> Int {
+        /**
+         我们来假设自己来购买股票。随着时间的推移，每天我们都可以选择出售股票与否。那么，假设在第 i 天，如果我们要在今天卖股票，那么我们能赚多少钱呢？
+
+         显然，如果我们真的在买卖股票，我们肯定会想：如果我是在历史最低点买的股票就好了！太好了，在题目中，我们只要用一个变量记录一个历史最低价格 minprice，我们就可以假设自己的股票是在那天买的。那么我们在第 i 天卖出股票能得到的利润就是 prices[i] - minprice。
+
+         因此，我们只需要遍历价格数组一遍，记录历史最低点，然后在每一天考虑这么一个问题：如果我是在历史最低点买进的，那么我今天卖出能赚多少钱？当考虑完所有天数之时，我们就得到了最好的答案。
+
+         链接：https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/solution/121-mai-mai-gu-piao-de-zui-jia-shi-ji-by-leetcode-/
+         */
+        var minValue = Int.max
+        var maxProfit = 0
+        for item in prices {
+            if item < minValue {
+                minValue = item
+            } else if (item - minValue > maxProfit) {
+                maxProfit = item - minValue
+            }
+        }
+        return maxProfit
+    }
     
     
     
