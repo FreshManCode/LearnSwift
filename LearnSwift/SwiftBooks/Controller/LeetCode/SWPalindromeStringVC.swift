0327d683f545d1e857cae7a1c40998cf14f6ee9c
zhangjunjun <zhangjunjun@weibopay.com>
2021-08-20 10:09:40 +0800

    LeetCode_只出现一次的数字

--- LearnSwift/SwiftBooks/Controller/LeetCode/SWPalindromeStringVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWPalindromeStringVC.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class SWPalindromeStringVC: SWBaseViewController {
     
+    let singleNums1 =  [2,2,1]
+    let singleNums2 =  [4,1,2,1,2]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -26,6 +29,14 @@ class SWPalindromeStringVC: SWBaseViewController {
         listItemArray.append(SWBookListItem(title: "1.1 验证回文串(优化) ",
                                             subTitle: "",
                                             funName: "modifyPalindrome"))
+        listItemArray.append(SWBookListItem(title: "2.只出现一次的数字 ",
+                                            subTitle: "",
+                                            funName: "singleNumber"))
+        listItemArray.append(SWBookListItem(title: "2.1 只出现一次的数字优化 ",
+                                            subTitle: "",
+                                            funName: "modifySingleNumber"))
+        
+        
         
         
         
@@ -138,5 +149,77 @@ class SWPalindromeStringVC: SWBaseViewController {
         }
         
     }
+    
+    
+    
+    // MARK: - 只出现一次的数字
+    @objc func singleNumber()  {
+        /**
+         给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+         
+         说明：
+         
+         你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+         
+         示例 1:
+         
+         输入: [2,2,1]
+         输出: 1
+         示例 2:
+         
+         输入: [4,1,2,1,2]
+         输出: 4
+         
+         链接：https://leetcode-cn.com/problems/single-number
+         */
+        
+        func caculateSingleNumber(_ nums: [Int]) -> Int {
+            var tempDic = [Int:Int]()
+            for item in nums {
+                if tempDic[item] != nil {
+                    tempDic.removeValue(forKey: item)
+                } else {
+                    tempDic[item] = item
+                }
+            }
+            return tempDic.keys.first!
+        }
+        
+        print("result1:\(caculateSingleNumber(singleNums1))")
+        print("result2:\(caculateSingleNumber(singleNums2))")
+
+    }
+    
+    // MARK: - 2.1 只出现一次的数字优化
+    @objc func modifySingleNumber()  {
+        /**
+         使用位运算。对于这道题，可使用异或运算 ⊕。异或运算有以下三个性质。
+
+         任何数和 0 做异或运算，结果仍然是原来的数，即 a⊕0=a。
+         任何数和其自身做异或运算，结果是 0，即 a⊕a=0。
+         异或运算满足交换律和结合律，即 a⊕b⊕a=b⊕a⊕a=b⊕(a⊕a)=b⊕0=b。
+
+         链接：https://leetcode-cn.com/problems/single-number/solution/zhi-chu-xian-yi-ci-de-shu-zi-by-leetcode-solution/
+         
+         */
+        func caculateSingleNumber(_ nums: [Int]) -> Int {
+            var single = 0
+            for item in nums {
+//              使用异或运算符
+                single = single ^ item
+            }
+            return single
+            
+        }
+        
+        print("result1:\(caculateSingleNumber(singleNums1))")
+        print("result2:\(caculateSingleNumber(singleNums2))")
+        
+    }
+
+    
+    
+    
+    
     
 }
