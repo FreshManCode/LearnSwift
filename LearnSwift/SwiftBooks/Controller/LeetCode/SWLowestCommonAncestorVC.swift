eb161ab92d17cda900f5cccd00e19b77b9fbc41b
zhangjunjun <zhangjunjun@weibopay.com>
2021-09-17 14:17:40 +0800

    LeetCode_二叉树的所有路径

--- LearnSwift/SwiftBooks/Controller/LeetCode/SWLowestCommonAncestorVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWLowestCommonAncestorVC.swift
@@ -36,6 +36,11 @@ class SWLowestCommonAncestorVC: SWBaseViewController {
         listItemArray.append(SWBookListItem(title: "3.1 有效的字母异位词(哈希映射,效率高) ",
                                             subTitle: "",
                                             funName: "isAnagram"))
+        listItemArray.append(SWBookListItem(title: "4. 二叉树的所有路径 ",
+                                            subTitle: "",
+                                            funName: "myBinaryTreePaths"))
+        
+        
         
         
         
@@ -244,6 +249,61 @@ class SWLowestCommonAncestorVC: SWBaseViewController {
         }
         
     }
+    
+    
+    // MARK: - 二叉树的所有路径
+    @objc func myBinaryTreePaths()  {
+        /**
+         给你一个二叉树的根节点 root ，按 任意顺序 ，返回所有从根节点到叶子节点的路径。
+
+         叶子节点 是指没有子节点的节点。
+         
+         输入：root = [1,2,3,null,5]
+         输出：["1->2->5","1->3"]
+         */
+        /**
+         方法一：深度优先搜索
+         思路与算法
+
+         最直观的方法是使用深度优先搜索。在深度优先搜索遍历二叉树时，我们需要考虑当前的节点以及它的孩子节点。
+
+         如果当前节点不是叶子节点，则在当前的路径末尾添加该节点，并继续递归遍历该节点的每一个孩子节点。
+         如果当前节点是叶子节点，则在当前路径末尾添加该节点后我们就得到了一条从根节点到叶子节点的路径，将该路径加入到答案即可。
+
+         链接：https://leetcode-cn.com/problems/binary-tree-paths/solution/er-cha-shu-de-suo-you-lu-jing-by-leetcode-solution/
+         */
+        func caculatePath(_ node:TreeNode?,_ path:String,_ result : inout [String]) {
+            if node != nil {
+                let letNode   = node?.left
+                let rightNode = node?.right
+                var path = path
+                path += "\(node!.val)"
+                // 当前节点是叶子节点
+                if letNode == nil && rightNode == nil {
+                    result.append(path)
+                }
+                // 当前节点不是叶子节点，继续递归遍历
+                else {
+                    path += "->"
+                    caculatePath(letNode, path, &result)
+                    caculatePath(rightNode, path, &result)
+                }
+            }
+        }
+        func binaryTreePaths(_ root: TreeNode?) -> [String] {
+            var result = [String]()
+            caculatePath(root, "", &result)
+            return result
+        }
+        
+        let treeNode1 = TreeNode(1, TreeNode(2, nil, TreeNode(5)), TreeNode(3))
+        let treeNode2 = TreeNode(1, TreeNode(2, TreeNode(3, TreeNode(5), TreeNode(6)), TreeNode(5)), TreeNode(3))
+        
+        print("treeNode1:\(binaryTreePaths(treeNode1))")
+        print("treeNode2:\(binaryTreePaths(treeNode2))")
+        
+    }
+
     
     
     
