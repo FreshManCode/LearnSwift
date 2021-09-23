97eebda0a24fe44d695db0c423da25d3303d1991
zhangjunjun <>
2021-09-23 09:41:29 +0800

    LeetCode_移动零

--- LearnSwift/SwiftBooks/Controller/LeetCode/SWLeetCodeListVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWLeetCodeListVC.swift
@@ -84,7 +84,7 @@ class SWLeetCodeListVC: SWBaseViewController {
         listItemArray.append(SWBookListItem(title: "23.二叉搜索树的最近公共祖先/删除链表中的节点/有效的字母异位词/二叉树的所有路径/各位相加/丑数",
                                             subTitle: "",
                                             funName: "SWLowestCommonAncestor"))
-        listItemArray.append(SWBookListItem(title: "24. 丢失的数字/",
+        listItemArray.append(SWBookListItem(title: "24. 丢失的数字/第一个错误的版本/移动零",
                                             subTitle: "",
                                             funName: "SWMissingNumber"))
         
--- LearnSwift/SwiftBooks/Controller/LeetCode/SWMissingNumberVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWMissingNumberVC.swift
@@ -29,13 +29,16 @@ class SWMissingNumberVC: SWBaseViewController {
                                             subTitle: "",
                                             funName: "myFirstBadVersion"))
         
+        listItemArray.append(SWBookListItem(title: "3.  移动零",
+                                            subTitle: "",
+                                            funName: "myMoveZeros"))
+        
+        listItemArray.append(SWBookListItem(title: "3.1  移动零(双指针)",
+                                            subTitle: "",
+                                            funName: "moveZeros"))
         
         
-        
-        
-        
-        
-        
+
         
         tableView.reloadData()
     }
@@ -125,6 +128,72 @@ class SWMissingNumberVC: SWBaseViewController {
         }
     }
     
+    
+    // MARK: - 移动零
+    @objc func myMoveZeros()  {
+        /**
+         给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+         输入: [0,1,0,3,12]
+         输出: [1,3,12,0,0]
+         */
+        func moveZeroes(_ nums: inout [Int]) {
+            var zeroNumbers = [Int]()
+            var deleteCount = 0
+            
+            for (index,item) in nums.enumerated() {
+                if item == 0 {
+                    let zeroNum = nums.remove(at: index - deleteCount)
+                    zeroNumbers.append(zeroNum)
+                    deleteCount += 1
+                }
+            }
+            nums.insert(contentsOf: zeroNumbers, at: nums.count)
+        }
+        var inputs1 = [0,1,0,3,12]
+        moveZeroes(&inputs1)
+        print("inputs1:\(inputs1)")
+    }
+    
+    // MARK: - 移动零(双指针)
+    @objc func moveZeros()  {
+        /**
+         使用双指针，左指针指向当前已经处理好的序列的尾部，右指针指向待处理序列的头部。
+
+         右指针不断向右移动，每次右指针指向非零数，则将左右指针对应的数交换，同时左指针右移。
+
+         注意到以下性质：
+
+         左指针左边均为非零数；
+
+         右指针左边直到左指针处均为零。
+
+         因此每次交换，都是将左指针的零与右指针的非零数交换，且非零数的相对顺序并未改变。
+
+         链接：https://leetcode-cn.com/problems/move-zeroes/solution/yi-dong-ling-by-leetcode-solution/
+         */
+        func swap(_ nums:inout [Int],_ left:Int,_ right:Int) {
+            let tempValue = nums[left]
+            nums[left]    = nums[right]
+            nums[right]   = tempValue
+        }
+        
+        func moveZeroes(_ nums: inout [Int]) {
+            var left = 0,right = 0;
+            while right < nums.count {
+                if nums[right] != 0 {
+                    swap(&nums, left, right)
+                    left += 1
+                }
+                right += 1
+            }
+        }
+        var inputs1 = [0,1,0,3,12]
+        moveZeroes(&inputs1)
+        print("inputs1:\(inputs1)")
+        
+    }
+
+
     
     
 }
