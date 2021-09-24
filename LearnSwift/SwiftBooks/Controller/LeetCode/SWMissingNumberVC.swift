5d4b400ad9feffba83cb79217ea4b682be98ab0c
zhangjunjun <zhangjunjun@weibopay.com>
2021-09-24 10:40:42 +0800

    LeetCode_Nim 游戏

--- LearnSwift/SwiftBooks/Controller/LeetCode/SWMissingNumberVC.swift
+++ LearnSwift/SwiftBooks/Controller/LeetCode/SWMissingNumberVC.swift
@@ -41,7 +41,12 @@ class SWMissingNumberVC: SWBaseViewController {
                                             subTitle: "",
                                             funName: "myWordPattern"))
         
+        listItemArray.append(SWBookListItem(title: "5.   Nim 游戏",
+                                            subTitle: "",
+                                            funName: "myCanWinNim"))
         
+        
+       
         
         tableView.reloadData()
     }
@@ -290,6 +295,26 @@ class SWMissingNumberVC: SWBaseViewController {
         print("2:\(wordPattern("abba", "dog cat cat fish"))")
         
     }
+    
+    // MARK: -Nim 游戏
+    @objc func myCanWinNim()  {
+        /**
+         你和你的朋友，两个人一起玩 Nim 游戏：
+
+         桌子上有一堆石头。
+         你们轮流进行自己的回合，你作为先手。
+         每一回合，轮到的人拿掉 1 - 3 块石头。
+         拿掉最后一块石头的人就是获胜者。
+         假设你们每一步都是最优解。请编写一个函数，来判断你是否可以在给定石头数量为 n 的情况下赢得游戏。如果可以赢，返回 true；否则，返回 false 。
+         */
+        func canWinNim(_ n: Int) -> Bool {
+            /**
+             分析:如果剩余的只要是4的整数倍,则我们无论怎样取,始终无法获取最后一块石头
+             */
+            return n % 4 != 0
+        }
+    }
+
 
 
 
